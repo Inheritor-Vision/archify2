e001fda8fbbc3544e2b37b829e78047a5fc4a197
Inheritor-Vision <valentin.boulou@gmail.com>
2023-02-10 23:29:10 +0100

    Cargo.toml & main.rs: Create client for spotify API usage. Possibility to use a Proxy on 127.0.0.1:8080

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,12 @@ use serde_json::Value;
 use single_instance::SingleInstance;
 use reqwest::header;
 
+static APP_USER_AGENT: &str = concat!(
+	env!("CARGO_PKG_NAME"),
+	"/",
+	env!("CARGO_PKG_VERSION")
+);
+
 struct ArchifyConf{
 	archify_id: String,
 }
@@ -48,12 +54,36 @@ fn verify_single_instance() -> SingleInstance{
 	instance
 }
 
+fn create_client(default_header: header::HeaderMap) -> reqwest::Client {
+	let client: reqwest::ClientBuilder;
+
+
+	#[cfg(not(feature = "proxy"))]{
+		client = reqwest::Client::builder()
+			.user_agent(APP_USER_AGENT)
+			.default_headers(default_header);
+	}
+	
+	#[cfg(feature = "proxy")]{
+		client = Client::builder()
+			.user_agent(APP_USER_AGENT)
+			.default_headers(default_header)
+			.proxy(reqwest::Proxy::http("http://127.0.0.1:8080").unwrap())
+			.proxy(reqwest::Proxy::https("http://127.0.0.1:8080").unwrap())
+			.add_root_certificate(get_certificate());
+	} 
+
+	client.build().unwrap()
+}
+
 fn main() {
    println!("Welcome to archify!");
    let _instance  = verify_single_instance();
 
    let conf = extract_configuration();
    let default_spot_header = create_spotify_api_header();
+
+   let spotify_client = create_client(default_spot_header);
 
    let args = arguments::parse_args();
 
