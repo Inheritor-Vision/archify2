60c5ec2808e8dc139e35ef44b45d87a9a2f7e943
Inheritor-Vision <valentin.boulou@gmail.com>
2023-02-10 21:04:38 +0100

    main.ts: Test for retreiving api keys

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+use std::{fs::File, io::Read};
+
+use serde_json::Value;
+
+struct ArchifyApi{
+	archify_id: String,
+	archify_secret: String
+}
+
+fn extract_configuration() -> ArchifyApi{
+	let mut buf = String::new();
+
+	File::open("data/secrets.json")
+	.unwrap()
+	.read_to_string(&mut buf)
+	.unwrap();
+
+	let json_api: Value = serde_json::from_str(&*buf).unwrap();
+
+	ArchifyApi { 
+		archify_id: json_api["archify_id"].to_string(), 
+		archify_secret: json_api["archify_secret"].to_string()
+	}
+}
+
 fn main() {
-   println!("Welcome to archify!"); 
+   println!("Welcome to archify!");
+
+   let api = extract_configuration();
+
+   println!("API_ID:		{}", api.archify_id);
+   println!("API_SECRET:	{}", api.archify_secret);
 }
