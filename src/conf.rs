375b0081666f439282144cd43fccb54702f1ad59
Inheritor-Vision <valentin.boulou@gmail.com>
2023-02-11 11:38:23 +0100

    All: Db first implem

--- src/conf.rs
+++ src/conf.rs
@@ -1,3 +1,6 @@
 // Dynamic Configuration Fields
 pub const CONF_ARCHIFY_ID: &str			= "archify_id";
 pub const CONF_ARCHIFY_SECRET: &str		= "archify_secret";
+
+// PATH
+pub const CONF_DATABASE_PATH: &str		= "data/db.sqlite";
--- src/database.rs
+++ src/database.rs
@@ -9,10 +9,19 @@ pub struct Database {
 
 impl Database {
 	pub fn new() -> Self {
-		let con = Connection::open(CONF_DATABASE_PATH).unwrap();
+		let con = Connection::open(CONF_DATABASE_PATH).unwrap();		
 
-		Database { 
+		let db  = Database { 
 			client: con 
-		}
+		};
+
+		db.create_tables();
+
+		db
+	}
+
+	fn create_tables(&self){
+		self.client.execute("CREATE TABLE IF NOT EXISTS spotify_app_tokens (access_token TEXT, token_type TEXT, duration BIGINT, received_at TIMESTAMP)", ()).unwrap();
+		self.client.execute("CREATE TABLE IF NOT EXISTS playlists (playlist_id TEXT, playlist_SHA256 BYTEA, timestamp TIMESTAMP, playlist_data TEXT, PRIMARY KEY (playlist_id, timestamp))", ()).unwrap();
 	}
 }
